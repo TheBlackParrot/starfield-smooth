3e778be3a3271682231de88141ac9ef75aa25b7f
TheBlackParrot <theblackparrot0@gmail.com>
2018-02-07 00:26:50 -0600

    mods

--- src/main.c
+++ src/main.c
@@ -1,17 +1,20 @@
 #include <pebble.h>
 
-#define MAX_STARS 60
-#define DELTA 33
+#define MAX_STARS 27
+#define DELTA 50
 
 typedef struct {
   short x;
   short y;
   short radius;
   bool visible;
+  bool planet;
+  GColor color;
 } Star;
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 static Layer *s_canvas;
 
 static GSize s_screen_size;
@@ -19,6 +22,9 @@ static AppTimer *s_render_timer;
 static Star *s_stars[MAX_STARS];
 
 static int s_visible_stars = 0;
+
+GColor planet_colors[5] = {GColorRed, GColorRajah, GColorBrightGreen, GColorBlueMoon, GColorShockingPink};
+GColor time_colors[5] = {GColorVividCerulean, GColorElectricBlue, GColorMediumSpringGreen, GColorYellow, GColorRichBrilliantLavender};
 
 /********************************** Star Lifecycle ****************************/
 
@@ -28,6 +34,8 @@ static Star* star_create() {
   this->y = 0;
   this->radius = 1;
   this->visible = false;
+  this->planet = false;
+  this->color = GColorWhite;
   return this;
 }
 
@@ -66,7 +74,12 @@ static void update() {
       s_stars[i]->visible = true;
       s_stars[i]->x = 0;
       s_stars[i]->y = rand() % s_screen_size.h;  // Between 0 and s_screen_size.h
-      s_stars[i]->radius = rand() % 5;
+      s_stars[i]->radius = rand() % 7;
+      if(s_stars[i]->radius >= 6 && rand() % 4 == 2) {
+        s_stars[i]->radius = 8 + rand() % 4;
+        s_stars[i]->planet = true;
+        s_stars[i]->color = planet_colors[rand() % 4];
+      }
       
       if(s_stars[i]->radius < 1) {
         s_stars[i]->radius++;
@@ -80,6 +93,8 @@ static void update() {
     // Evaluate visibility
     if(s_stars[i]->x >= s_screen_size.w + s_stars[i]->radius) {
       s_stars[i]->visible = false;  // Dissapears for recycling
+      s_stars[i]->planet = false;
+      s_stars[i]->color = GColorWhite;
       s_visible_stars--;
     }
     
@@ -107,11 +122,10 @@ static void start_rendering() {
 }
 
 static void render_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorWhite);
-
   // Draw Star.radius rects for all s_stars
   for(int i = 0; i < MAX_STARS; i++) {
     if(s_stars[i]->visible) {
+      graphics_context_set_fill_color(ctx, s_stars[i]->color);
       graphics_fill_rect(ctx, GRect(s_stars[i]->x, s_stars[i]->y, s_stars[i]->radius, s_stars[i]->radius), 0, GCornerNone);
     }
   }
@@ -119,10 +133,27 @@ static void render_update_proc(Layer *layer, GContext *ctx) {
 
 /****************************** Window Lifecycle *****************************/
 
+char *upcase(char *str) {
+  for (int i = 0; str[i] != 0; i++) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      str[i] -= 0x20;
+    }
+  }
+
+  return str;
+}
+
 static void set_time_display(struct tm *t) {
+  int col = rand() % 5;
+  text_layer_set_text_color(s_time_layer, time_colors[col]);
+  text_layer_set_text_color(s_date_layer, time_colors[col]);
+
 	static char s_time_buffer[8];
-  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", t);
+  static char s_date_buffer[16];
+  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H\r\n%M" : "%I\r\n%M", t);
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %e", t);
   text_layer_set_text(s_time_layer, s_time_buffer);
+  text_layer_set_text(s_date_layer, upcase(s_date_buffer));
 }
 
 static void window_load(Window *window) {
@@ -136,11 +167,16 @@ static void window_load(Window *window) {
   s_canvas = layer_create(bounds);
   layer_set_update_proc(s_canvas, render_update_proc);
   layer_add_child(window_layer, s_canvas);
-  
+
   // Setup time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 60, s_screen_size.w, 50));
-  text_layer_set_font(s_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_38)));
-  text_layer_set_text_color(s_time_layer, GColorWhite);
+  s_date_layer = text_layer_create(GRect(1, 12, bounds.size.w-12, 30));
+  text_layer_set_font(s_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_AERX_18)));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentRight);
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+
+  s_time_layer = text_layer_create(GRect(1, bounds.size.h-61, 60, 60));
+  text_layer_set_font(s_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GAMEPLAY_24)));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
@@ -157,6 +193,7 @@ static void window_unload(Window *window) {
   app_timer_cancel(s_render_timer);
   
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
   layer_destroy(s_canvas);
 }
 
